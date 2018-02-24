c690557bb4af1d971e6c78821c994c20abc885dd
Emman-PC\Emman <kioskigal@gmail.com>
2018-02-24 19:20:49 +0800

    revise the test for login logout added wait

--- Tommy_SmokeTest.py
+++ Tommy_SmokeTest.py
@@ -18,24 +18,38 @@ class  Verify_Share_Via_Social_Media(unittest.TestCase):
         driver = self.driver
         driver.get("https://app.mytommy.com/")
         
+        try:
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "login_username")))
+        finally:
 
-        driver.implicitly_wait(10)
-        driver.find_element_by_name("username").send_keys("eagalleto@gmail.com")
-        driver.implicitly_wait(5)
-        driver.find_element_by_class_name("button-fill").click();
+            driver.find_element_by_name("username").send_keys("eagalleto@gmail.com")
+            driver.find_element_by_class_name("button-fill").click();
 
-        assert "eagalleto@gmail.com" in driver.find_element_by_name("username").get_attribute("value")
+            self.assertTrue("eagalleto@gmail.com" in driver.find_element_by_name("username").get_attribute("value"))
 
-        driver.implicitly_wait(5)
-        driver.find_element_by_class_name("password").send_keys("Tommy123");
-        driver.find_element_by_class_name("submit-login").click();
-        self.assertTrue("Chats" in driver.find_element_by_class_name("chat-tab-link").get_attribute("innerText"))
+        try:
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "submit-login")))
+        finally:
+            driver.find_element_by_class_name("password").send_keys("Tommy123");
+            driver.find_element_by_class_name("submit-login").click();
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "icon-chat")))
+        finally:
+            self.assertTrue("Chats" in driver.find_element_by_class_name("chat-tab-link").get_attribute("innerText"))
 
-        driver.find_element_by_id("main-menu-drag-handle").click();
-        driver.find_element_by_xpath("//*[@id=\"main-menu-administration\"]/div[2]/ul/li[1]/a").click();
-        driver.find_element_by_class_name("logout-button").click();
+            driver.find_element_by_id("main-menu-drag-handle").click();
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"main-menu-administration\"]/div[2]/ul/li[1]/a")))
+        finally:
+            driver.find_element_by_xpath("//*[@id=\"main-menu-administration\"]/div[2]/ul/li[1]/a").click();
 
-        self.assertEqual("Email address / Phone", driver.find_element_by_class_name("username").get_attribute("placeholder"))
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "logout-button")))
+        finally:
+
+            driver.find_element_by_class_name("logout-button").click();
+
+            self.assertEqual("Email address / Phone", driver.find_element_by_class_name("username").get_attribute("placeholder"))
 
     def tearDown(self):
         self.driver.close()
