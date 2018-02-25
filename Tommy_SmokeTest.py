c4610c0d2581bacabb99a480f71b3c25ebfafdea
Emman-PC\Emman <kioskigal@gmail.com>
2018-02-25 20:37:16 +0800

    put the test to pycharm editor

--- Tommy_SmokeTest.py
+++ Tommy_SmokeTest.py
@@ -1,53 +1,56 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 
-class  Verify_Share_Via_Social_Media(unittest.TestCase):
+
+class TommyTests(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
-        
-        
 
-    def test_Open_Website(self):
+    def test_login_logout(self):
 
         # self.driver.maximize_window()
         driver = self.driver
         driver.get("https://app.mytommy.com/")
-        
+
         try:
-            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "login_username")))
+            WebDriverWait(driver, 15).until(expected_conditions.presence_of_element_located((By.ID, "login_username")))
         finally:
 
             driver.find_element_by_name("username").send_keys("eagalleto@gmail.com")
-            driver.find_element_by_class_name("button-fill").click();
+            driver.find_element_by_class_name("button-fill").click()
 
             self.assertTrue("eagalleto@gmail.com" in driver.find_element_by_name("username").get_attribute("value"))
 
         try:
-            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "submit-login")))
+            WebDriverWait(driver, 15).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "submit-login")))
         finally:
-            driver.find_element_by_class_name("password").send_keys("Tommy123");
-            driver.find_element_by_class_name("submit-login").click();
+            driver.find_element_by_class_name("password").send_keys("Tommy123")
+            driver.find_element_by_class_name("submit-login").click()
         try:
-            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "icon-chat")))
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "icon-chat")))
         finally:
             self.assertTrue("Chats" in driver.find_element_by_class_name("chat-tab-link").get_attribute("innerText"))
 
-            driver.find_element_by_id("main-menu-drag-handle").click();
-        try:
-            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"main-menu-administration\"]/div[2]/ul/li[1]/a")))
-        finally:
-            driver.find_element_by_xpath("//*[@id=\"main-menu-administration\"]/div[2]/ul/li[1]/a").click();
+            driver.find_element_by_id("main-menu-drag-handle").click()
 
         try:
-            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "logout-button")))
+
+            WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, "menu-label"), "Chats"))
+
         finally:
 
-            driver.find_element_by_class_name("logout-button").click();
+            (driver.find_element_by_id("main-menu-administration").find_elements_by_tag_name("img"))[0].click()
+
+
+        try:
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "logout-button")))
+        finally:
+
+            driver.find_element_by_class_name("logout-button").click()
 
             self.assertEqual("Email address / Phone", driver.find_element_by_class_name("username").get_attribute("placeholder"))
 
@@ -55,5 +58,5 @@ class  Verify_Share_Via_Social_Media(unittest.TestCase):
         self.driver.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
