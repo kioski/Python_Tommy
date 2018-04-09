dbb2ca8f3a2724bb14ecf1bdb060f67616235064
Emman-PC\Emman <kioskigal@gmail.com>
2018-04-09 17:03:28 +0800

    Added Error handling for assert

--- Tommy_SmokeTest.py
+++ Tommy_SmokeTest.py
@@ -16,7 +16,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_1LoginAndLogout(self):
+    def test_1aLoginAndLogout(self):
 
         driver = self.driver
 
@@ -39,8 +39,13 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_LogoutButton), "Logout"))
         driver.find_element_by_class_name(SelectorClass_LogoutButton).click()
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_NextButton)))
-        
-        self.assertEqual("Email address / Phone", driver.find_element_by_class_name(SelectorName_Username).get_attribute("placeholder"))
+
+        try:
+            self.assertEqual("Email address / Phone", driver.find_element_by_class_name(SelectorName_Username).get_attribute("placeholder"))
+
+        except AssertionError as Error:
+
+            print("Error_1: " + str(Error))
 
     def tearDown(self):
         self.driver.close()
@@ -48,7 +53,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_2Contacts_NewChat(self):
+    def test_1bContacts_NewChat(self):
         
         driver = self.driver
 
@@ -68,12 +73,19 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_CheckBox)))
         driver.find_element_by_class_name(SelectorClass_AddNewMenu_NewChatOkButton).click()
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, SelectorId_AddNewMenu_TextBox)))
-        driver.find_element_by_id(SelectorId_AddNewMenu_TextBox).send_keys("This is a test text")
+        Sample_Text = str(uuid.uuid1())[:8]
+        driver.find_element_by_id(SelectorId_AddNewMenu_TextBox).send_keys(Sample_Text)
         (driver.find_element_by_class_name(SelectorClass_AddNewMenu_TextBoxSendBUtton).find_elements_by_tag_name("a"))[0].click()
-        WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_InputtedText), "This is a test text"))
+        time.sleep(3)
       
+
         # Need to delete always the text
-        self.assertEqual("This is a test text", driver.find_element_by_class_name(SelectorClass_AssertInputtedText).find_elements_by_tag_name("div")[1].get_attribute("innerText"))
+        try:
+            self.assertEqual(Sample_Text, driver.find_element_by_class_name(SelectorClass_AssertInputtedText).find_elements_by_tag_name("div")[1].get_attribute("innerText"))
+
+        except AssertionError as Error:
+
+            print("Error_2: " + str(Error))
 
 
     def tearDown(self):
@@ -83,7 +95,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_3Contacts_AddContacts_SMS(self):
+    def test_1cContacts_AddContacts_SMS(self):
         
         driver = self.driver
 
@@ -110,8 +122,15 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
         WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Tommy"))
 
+
         # Confirmation not correct. It should say Sent or Confirm
-        self.assertEqual("Tommy", driver.find_element_by_class_name(SelectorClass_AddNewMenu_InviteSMS_Confirmation).find_elements_by_tag_name("div")[0].get_attribute("innerText"))
+        try:
+            self.assertEqual("Sent", driver.find_element_by_class_name(SelectorClass_AddNewMenu_InviteSMS_Confirmation).find_elements_by_tag_name("div")[1].get_attribute("innerText"))
+
+        except AssertionError as Error:
+
+            print("Error_3: " + str(Error))
+        
 
 
     def tearDown(self):
@@ -120,7 +139,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_4Contacts_AddContacts_Email(self):
+    def test_1dContacts_AddContacts_Email(self):
         
         driver = self.driver
 
@@ -143,11 +162,16 @@ class TommyTests(unittest.TestCase):
         driver.find_element_by_name(SelectorName_AddNewMenu_InviteEmail_Name).send_keys("Tommy Test")
         driver.find_element_by_name(SelectorName_AddNewMenu_InviteEmail_Email).send_keys(str(uuid.uuid1())+"@gmail.com")
         (driver.find_element_by_class_name(SelectorClass_AddNewMenu_InviteSMS_Send).find_elements_by_tag_name("span"))[1].click()
-        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
-        WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
-        WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Tommy"))
 
-        # No Confirmation Dialog Box. Wait must be consider        
+        # No Confirmation Dialog Box. Wait must be consider   
+        try:
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
+            WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
+            WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Tommy"))
+        
+        except:
+
+            print("Error_4: No Dialog Box Confirmation")
 
 
     def tearDown(self):
@@ -156,7 +180,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_5Contacts_AddContacts_InvitationLink(self):
+    def test_1eContacts_AddContacts_InvitationLink(self):
         
         driver = self.driver
 
@@ -180,7 +204,14 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Invitation Link"))
         driver.get(driver.find_element_by_name(SelectorName_AddNewMenu_InviteEmail_Link).get_attribute("value"))
 
-        self.assertTrue("Welcome to Tommy!" in driver.page_source)
+        try:
+            self.assertTrue("Welcome to Tommy!" in driver.page_source)
+
+        except AssertionError as Error:
+
+            print("Error_5: " + str(Error))
+        
+        
 
 
     def tearDown(self):
@@ -189,7 +220,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_6AddTeam(self):
+    def test_1fAddTeam(self):
         
         driver = self.driver
 
@@ -213,7 +244,13 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, SelectorClass_AddTeam_Notification)))
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_AddTeam_Notification)))
 
-        self.assertEqual("Your team account has been created!", (driver.find_element_by_class_name(SelectorClass_AddTeam_Notification).find_elements_by_tag_name("div"))[5].get_attribute("innerText"))
+        try:
+            self.assertEqual("Your team account has been created!", (driver.find_element_by_class_name(SelectorClass_AddTeam_Notification).find_elements_by_tag_name("div"))[5].get_attribute("innerText"))
+
+        except AssertionError as Error:
+
+            print("Error_6: " + str(Error))
+        
 
 
     def tearDown(self):
@@ -221,7 +258,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_7About_InviteAFriendSMS(self):
+    def test_1gAbout_InviteAFriendSMS(self):
 
         driver = self.driver
 
@@ -248,15 +285,21 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
         WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Tommy"))
 
-        # Confirmation not correct. It should say Sent or Confirm
-        self.assertEqual("Tommy", driver.find_element_by_class_name(SelectorClass_AddNewMenu_InviteSMS_Confirmation).find_elements_by_tag_name("div")[0].get_attribute("innerText"))
+        # Confirmation not correct. It should say Sent or Confirm   
+        try:
+            self.assertEqual("Sent", driver.find_element_by_class_name(SelectorClass_AddNewMenu_InviteSMS_Confirmation).find_elements_by_tag_name("div")[1].get_attribute("innerText"))
+
+        except AssertionError as Error:
+
+            print("Error_7: " + str(Error))
+        
 
     def tearDown(self):
         self.driver.close()
 
 
 
-    def test_8About_InviteAFriendEmail(self):
+    def test_1hAbout_InviteAFriendEmail(self):
 
         driver = self.driver
 
@@ -279,11 +322,17 @@ class TommyTests(unittest.TestCase):
         driver.find_element_by_name(SelectorName_AddNewMenu_InviteEmail_Name).send_keys("Tommy Test")
         driver.find_element_by_name(SelectorName_AddNewMenu_InviteEmail_Email).send_keys(str(uuid.uuid1())[:8]+"@gmail.com")
         (driver.find_element_by_class_name(SelectorClass_AddNewMenu_InviteSMS_Send).find_elements_by_tag_name("span"))[1].click()
-        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
-        WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
-        WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Tommy"))
 
-        # No Confirmation Dialog Box. Wait must be Wait  
+    #     # No Confirmation Dialog Box. Wait must be consider   
+        try:
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
+            WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBox)))
+            WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Tommy"))
+        
+        except:
+
+            print("Error_8: No Dialog Box Confirmation")
+
 
     def tearDown(self):
         self.driver.close()
@@ -291,7 +340,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_9About_InviteAFriendInvitelink(self):
+    def test_1iAbout_InviteAFriendInvitelink(self):
 
         driver = self.driver
 
@@ -315,7 +364,13 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_AddNewMenu_InviteSMS_DialogBoxTitle), "Invitation Link"))
         driver.get(driver.find_element_by_name(SelectorName_AddNewMenu_InviteEmail_Link).get_attribute("value"))
 
-        self.assertTrue("Welcome to Tommy!" in driver.page_source)
+        try:
+            self.assertTrue("Welcome to Tommy!" in driver.page_source)
+
+        except AssertionError as Error:
+
+            print("Error_9: " + str(Error))
+
 
     def tearDown(self):
         self.driver.close()
@@ -323,7 +378,7 @@ class TommyTests(unittest.TestCase):
 
 
 
-    def test_10About_Feedback(self):
+    def test_1jAbout_Feedback(self):
         
         driver = self.driver
 
@@ -342,14 +397,20 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, SelectorId_Feedback)))
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, SelectorId_Feedback)))
 
+        try:
+            self.assertEqual("Feedback", driver.find_element_by_id(SelectorId_Feedback).find_elements_by_tag_name("p")[0].get_attribute("innerText"))
 
-        self.assertEqual("Feedback", driver.find_element_by_id(SelectorId_Feedback).find_elements_by_tag_name("p")[0].get_attribute("innerText"))
+        except AssertionError as Error:
+
+            print("Error_10: " + str(Error))
+
+        
 
     def tearDown(self):
         self.driver.close()
 
 
-    def test_11About_Acknowledgements(self):
+    def test_1kAbout_Acknowledgements(self):
         
         driver = self.driver
 
@@ -368,19 +429,24 @@ class TommyTests(unittest.TestCase):
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, SelectorId_Feedback)))
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, SelectorId_Feedback)))
 
+        try:
+            self.assertEqual("Acknowledgements", driver.find_element_by_id(SelectorId_Feedback).find_elements_by_tag_name("p")[0].get_attribute("innerText"))
 
-        self.assertEqual("Acknowledgements", driver.find_element_by_id(SelectorId_Feedback).find_elements_by_tag_name("p")[0].get_attribute("innerText"))
+        except AssertionError as Error:
+
+            print("Error_11: " + str(Error))
+
+        
 
     def tearDown(self):
         self.driver.close()
 
 
 
-    def test_12About_Account(self):
+    def test_1lAbout_Account(self):
         
         driver = self.driver
 
-        #For Login
         WebDriverWait(driver, 15).until(expected_conditions.presence_of_element_located((By.ID, SelectorId_LoginUserName)))
         driver.find_element_by_name(SelectorName_Username).send_keys(Email_Add)
         driver.find_element_by_class_name(SelectorClass_NextButton).click()
@@ -388,10 +454,6 @@ class TommyTests(unittest.TestCase):
         driver.find_element_by_class_name(SelectorClass_LoginPassword).send_keys(Password)
         driver.find_element_by_class_name(SelectorClass_SubmitButton).click()
         WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, SelectorClass_ChatIcon)))
-        
-        self.assertEqual("Chats", driver.find_element_by_class_name(SelectorClass_ChatTitle).get_attribute("innerText"))
-
-        #For Logout
         driver.find_element_by_id(SelectorId_SidebarExpand).click()
         WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element((By.CLASS_NAME, SelectorClass_SideBarChatIcon), "Chats"))
         (driver.find_element_by_id(SelectorId_AdministrationMenu).find_elements_by_tag_name("img"))[0].click()
@@ -401,13 +463,26 @@ class TommyTests(unittest.TestCase):
         details = ("Emman",       "Galleto",   "1989-10-30<","eagalleto@gmail.com", "+6391233026697")
         selector = ("first_name", "last_name", "dob",         "email",               "mobile")
 
-        self.assertEqual(details[0], driver.find_element_by_name(selector[0]).get_attribute("value"))
-        self.assertEqual(details[1], driver.find_element_by_name(selector[1]).get_attribute("value"))
-        # Date of Birth value is empty
-        # self.assertEqual(details[2], driver.find_element_by_name(selector[2]).get_attribute("value"))
-        # Email is wrong its not eagalleto@gmail.com
-        # self.assertEqual(details[3], driver.find_element_by_name(selector[3]).get_attribute("value"))
-        self.assertEqual(details[4], driver.find_element_by_name(selector[4]).get_attribute("value"))
+        try:
+            self.assertEqual(details[0], driver.find_element_by_name(selector[0]).get_attribute("value"))
+        except AssertionError as Error:
+            print("Error_12: " + str(Error))
+        try:
+            self.assertEqual(details[1], driver.find_element_by_name(selector[1]).get_attribute("value"))
+        except AssertionError as Error:
+            print("Error_12: " + str(Error))
+        try:
+            self.assertEqual(details[2], driver.find_element_by_name(selector[2]).get_attribute("value"))
+        except AssertionError as Error:
+            print("Error_12: " + str(Error))
+        try:
+            self.assertEqual(details[3], driver.find_element_by_name(selector[3]).get_attribute("value"))
+        except AssertionError as Error:
+            print("Error_12: " + str(Error))
+        try:      
+            self.assertEqual(details[4], driver.find_element_by_name(selector[4]).get_attribute("value"))
+        except AssertionError as Error:
+            print("Error_12: " + str(Error))
 
 
     def tearDown(self):
