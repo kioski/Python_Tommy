cd4e9166f88fe61825683f43a665d524e9bfb557
Emman-PC\Emman <kioskigal@gmail.com>
2018-05-04 15:33:52 +0800

    Added Credentials

---
+++ Credentials.py
@@ -0,0 +1,17 @@
+desired_capabilities_android = {
+                 'browserName': 'android',
+                 'device': 'Samsung Galaxy S8',
+                 'realMobile': 'true',
+                 'os_version': '7.0',
+                 'browserstack.debug':'true'
+                }
+
+desired_capabilities_ios = {
+				 'browserName': 'iPhone',
+				 'device': 'iPhone 8',
+				 'realMobile': 'true',
+				 'os_version': '11.0',
+                 'browserstack.debug':'true'
+                }
+
+command_executor='http://masonfok1:yYkqqhe5936SdqbzsUxo@hub.browserstack.com:80/wd/hub'
