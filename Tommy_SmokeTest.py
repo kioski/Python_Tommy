7de072abb75ebc9533fc723ac181a962f7024c89
Egalleto <egalleto@transim.com>
2018-05-23 15:04:56 +0800

    restore import *

--- Tommy_SmokeTest.py
+++ Tommy_SmokeTest.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import time, random, string, uuid
-# from Keywords import *
-# from Credentials import *
+from Keywords import *
+from Credentials import *
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
